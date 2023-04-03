1c3e2273b95f19c43e142ad2cc1e8170b91b8efa
camiloesan <camiloesan@gmail.com>
2023-04-02 21:06:55 -0600

    dao structure

---
+++ src/main/java/mx/uv/fei/logic/AccessAccountDAO.java
@@ -0,0 +1,22 @@
+package mx.uv.fei.logic;
+
+import mx.uv.fei.dao.IAccessAccount;
+
+import java.sql.SQLException;
+
+public class AccessAccountDAO implements IAccessAccount {
+    @Override
+    public void addAccessAccount(AccessAccountDAO accessAccount) throws SQLException {
+
+    }
+
+    @Override
+    public void modifyAccessAccount(AccessAccountDAO accessAccount) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteAccessAccountByName(String name) throws SQLException {
+
+    }
+}
