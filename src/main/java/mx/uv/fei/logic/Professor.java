1c8c3702344a1c9559af5cbdd885cdfdfd045e58
Luis <zs21013875@estudiantes.uv.mx>
2023-04-02 23:16:57 -0600

    Professor class & DAO implementation

---
+++ src/main/java/mx/uv/fei/logic/Professor.java
@@ -0,0 +1,59 @@
+package mx.uv.fei.logic;
+
+public class Professor {
+    private String professorName;
+    private String professorFirsLastName;
+    private String professorSecondLastName;
+    private String professorEmail;
+    private int userId;
+
+    public Professor() {}
+
+    public Professor(String professorName, String professorFirsLastName, String professorSecondLastName, String professorEmail, int userId) {
+        this.professorName = professorName;
+        this.professorFirsLastName = professorFirsLastName;
+        this.professorSecondLastName = professorSecondLastName;
+        this.professorEmail = professorEmail;
+        this.userId = userId;
+    }
+
+    public String getProfessorName() {
+        return professorName;
+    }
+
+    public String getProfessorFirsLastName() {
+        return professorFirsLastName;
+    }
+
+    public String getProfessorSecondLastName() {
+        return professorSecondLastName;
+    }
+
+    public String getProfessorEmail() {
+        return professorEmail;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setProfessorName(String professorName) {
+        this.professorName = professorName;
+    }
+
+    public void setProfessorFirsLastName(String professorFirsLastName) {
+        this.professorFirsLastName = professorFirsLastName;
+    }
+
+    public void setProfessorSecondLastName(String professorSecondLastName) {
+        this.professorSecondLastName = professorSecondLastName;
+    }
+
+    public void setProfessorEmail(String professorEmail) {
+        this.professorEmail = professorEmail;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
