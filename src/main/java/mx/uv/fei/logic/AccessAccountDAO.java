d5c02b427eaf7b1547f1d9351382c99a1a7abcc8
camiloesan <camiloesan@gmail.com>
2023-04-02 22:35:42 -0600

    class writing

--- src/main/java/mx/uv/fei/logic/AccessAccountDAO.java
+++ src/main/java/mx/uv/fei/logic/AccessAccountDAO.java
@@ -1,14 +1,23 @@
 package mx.uv.fei.logic;
 
 import mx.uv.fei.dao.IAccessAccount;
-import mx.uv.fei.dao.IProjectEvidence;
+import mx.uv.fei.dataaccess.DatabaseManager;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AccessAccountDAO implements IAccessAccount {
     @Override
     public void addAccessAccount(AccessAccount accessAccount) throws SQLException {
+        String query = "insert into CuentasAcceso(ID_usuario, nombreUsuario, contrasena) values (?,?,?)";
+        DatabaseManager databaseManager = new DatabaseManager();
+        Connection connection = databaseManager.getConnection();
 
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, accessAccount.getUserId());
+        preparedStatement.setString(2, accessAccount.getUsername());
+        preparedStatement.setString(3, accessAccount.getUserPassword());
     }
 
     @Override
--- src/main/java/mx/uv/fei/logic/ProjectEvidence.java
+++ src/main/java/mx/uv/fei/logic/ProjectEvidence.java
@@ -1,9 +1,97 @@
 package mx.uv.fei.logic;
 
-import mx.uv.fei.dao.IProjectEvidence;
-
-import java.sql.SQLException;
-
 public class ProjectEvidence {
+    private int projectEvidenceId;
+    private String projectEvidenceTitle;
+    private String projectEvidenceStatus;
+    private int projectEvidenceGrade;
+    private String projectEvidenceDescription;
+    private int professorId;
+    private int progressId;
+    private int projectId;
+    private String studentMatricula;
 
+    public ProjectEvidence(int projectEvidenceId, String projectEvidenceTitle, String projectEvidenceStatus, int projectEvidenceGrade, String projectEvidenceDescription, int professorId, int progressId, int projectId, String studentMatricula) {
+        this.projectEvidenceId = projectEvidenceId;
+        this.projectEvidenceTitle = projectEvidenceTitle;
+        this.projectEvidenceStatus = projectEvidenceStatus;
+        this.projectEvidenceGrade = projectEvidenceGrade;
+        this.projectEvidenceDescription = projectEvidenceDescription;
+        this.professorId = professorId;
+        this.progressId = progressId;
+        this.projectId = projectId;
+        this.studentMatricula = studentMatricula;
+    }
+
+    public int getProjectEvidenceId() {
+        return projectEvidenceId;
+    }
+
+    public void setProjectEvidenceId(int projectEvidenceId) {
+        this.projectEvidenceId = projectEvidenceId;
+    }
+
+    public String getProjectEvidenceTitle() {
+        return projectEvidenceTitle;
+    }
+
+    public void setProjectEvidenceTitle(String projectEvidenceTitle) {
+        this.projectEvidenceTitle = projectEvidenceTitle;
+    }
+
+    public String getProjectEvidenceStatus() {
+        return projectEvidenceStatus;
+    }
+
+    public void setProjectEvidenceStatus(String projectEvidenceStatus) {
+        this.projectEvidenceStatus = projectEvidenceStatus;
+    }
+
+    public int getProjectEvidenceGrade() {
+        return projectEvidenceGrade;
+    }
+
+    public void setProjectEvidenceGrade(int projectEvidenceGrade) {
+        this.projectEvidenceGrade = projectEvidenceGrade;
+    }
+
+    public String getProjectEvidenceDescription() {
+        return projectEvidenceDescription;
+    }
+
+    public void setProjectEvidenceDescription(String projectEvidenceDescription) {
+        this.projectEvidenceDescription = projectEvidenceDescription;
+    }
+
+    public int getProfessorId() {
+        return professorId;
+    }
+
+    public void setProfessorId(int professorId) {
+        this.professorId = professorId;
+    }
+
+    public int getProgressId() {
+        return progressId;
+    }
+
+    public void setProgressId(int progressId) {
+        this.progressId = progressId;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getStudentMatricula() {
+        return studentMatricula;
+    }
+
+    public void setStudentMatricula(String studentMatricula) {
+        this.studentMatricula = studentMatricula;
+    }
 }
