e5f18edd16fc4ae8074501d9eb21904d149d9f83
camiloesan <camiloesan@gmail.com>
2023-04-15 14:19:12 -0600

    adxcxxc

--- src/main/java/mx/uv/fei/logic/AccessAccount.java
+++ src/main/java/mx/uv/fei/logic/AccessAccount.java
@@ -7,43 +7,9 @@ public class AccessAccount {
     private String username;
     private String userPassword;
     private String userType;
-    private SimpleStringProperty usernameProperty;
-    private SimpleStringProperty userTypeProperty;
-
-
 
     public AccessAccount() {
     }
-
-    public AccessAccount(String username, String userType) {
-        this.usernameProperty = new SimpleStringProperty(username);
-        this.userTypeProperty = new SimpleStringProperty(userType);
-    }
-
-    public String getUsernameProperty() {
-        return usernameProperty.get();
-    }
-
-    public SimpleStringProperty usernamePropertyProperty() {
-        return usernameProperty;
-    }
-
-    public void setUsernameProperty(String usernameProperty) {
-        this.usernameProperty.set(usernameProperty);
-    }
-
-    public String getUserTypeProperty() {
-        return userTypeProperty.get();
-    }
-
-    public SimpleStringProperty userTypePropertyProperty() {
-        return userTypeProperty;
-    }
-
-    public void setUserTypeProperty(String userTypeProperty) {
-        this.userTypeProperty.set(userTypeProperty);
-    }
-
     public String getUserType() {
         return userType;
     }
