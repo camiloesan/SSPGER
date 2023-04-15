7ae994df788d9ed5ab1250db598576b48b419ec0
Luis <zs21013875@estudiantes.uv.mx>
2023-04-14 23:17:41 -0600

    test classes for professorDAO & projectDAO

---
+++ src/test/java/mx/uv/fei/logic/ProjectDAOTest.java
@@ -0,0 +1,41 @@
+package mx.uv.fei.logic;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addProject() {
+    }
+
+    @Test
+    void testGetProjectsByState() throws SQLException {
+        System.out.println("Test getProjectsByState");
+        ProjectDAO projectDAO = new ProjectDAO();
+        projectDAO.getProjectsByState("Por revisar");
+
+        ArrayList<DetailedProject> expectedResult = new ArrayList<>();
+        ArrayList<DetailedProject> result = new ArrayList<>(projectDAO.getProjectsByState("Verificado"));
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void testUpdateProjectState() throws SQLException {
+        System.out.println("Test updateProjectState");
+    }
+}
