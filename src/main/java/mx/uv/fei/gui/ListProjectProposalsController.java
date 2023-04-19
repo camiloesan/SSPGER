c6d2be8d26b8fe8256af78fd28850eccebbd10fb
Luis <zs21013875@estudiantes.uv.mx>
2023-04-18 20:33:51 -0600

    open project details

--- src/main/java/mx/uv/fei/gui/ListProjectProposalsController.java
+++ src/main/java/mx/uv/fei/gui/ListProjectProposalsController.java
@@ -5,10 +5,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
-import javafx.scene.control.cell.PropertyValueFactory;
 import mx.uv.fei.dao.ProjectDAO;
 import mx.uv.fei.logic.DetailedProject;
-import mx.uv.fei.logic.Project;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -32,8 +30,11 @@ public class ListProjectProposalsController {
         ProjectDAO projectDAO = new ProjectDAO();
         listViewProjectProposals.getItems().clear();
         
-        ArrayList<DetailedProject> detailedProjects = new ArrayList<>(projectDAO.getProjectsByState("Por revisar"));
-        
-        detailedProjects.forEach(element -> listViewProjectProposals.getItems().add(element.getProjectTitle()));
+        ArrayList<DetailedProject> proposedProjects = new ArrayList<>(projectDAO.getProjectsByState("Por revisar"));
+        proposedProjects.forEach(element -> listViewProjectProposals.getItems().add(element.getProjectTitle()));
+    }
+    
+    public void openProjectDetails() throws SQLException {
+    
     }
 }
