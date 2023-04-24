5a5dedd5ee0884a3a932042de2184e16e735e77c
Luis <zs21013875@estudiantes.uv.mx>
2023-04-24 16:13:01 -0600

    Updated package imports

--- src/main/java/mx/uv/fei/gui/ListProjectProposalsController.java
+++ src/main/java/mx/uv/fei/gui/ListProjectProposalsController.java
@@ -3,7 +3,10 @@ package mx.uv.fei.gui;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
 
 import mx.uv.fei.dao.ProjectDAO;
 import mx.uv.fei.logic.DetailedProject;
