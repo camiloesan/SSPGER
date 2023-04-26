aadb7d5dfdc10f8115677e43f82682c3382e610b
Luis <zs21013875@estudiantes.uv.mx>
2023-04-25 23:42:55 -0600

    refactor filter parameters

--- src/main/java/mx/uv/fei/gui/ViewProjectProposalsController.java
+++ src/main/java/mx/uv/fei/gui/ViewProjectProposalsController.java
@@ -13,10 +13,11 @@ import mx.uv.fei.dao.ProjectDAO;
 import mx.uv.fei.logic.DetailedProject;
 
 import java.sql.SQLException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class ListProjectProposalsController {
+public class ViewProjectProposalsController {
     @FXML
     private Label labelHeader;
     @FXML
@@ -28,6 +29,14 @@ public class ListProjectProposalsController {
     @FXML
     private ListView<String> listViewProjects;
     
+    private static final String ALL_COMBO_OPTION = "Todos";
+    private static final String UNVERIFIED_COMBO_OPTION = "Por revisar";
+    private static final String VERIFIED_COMBO_OPTION = "Verificados";
+    private static final String DECLINED_COMBO_OPTION = "Declinados";
+    
+    private static final String UNVERIFIED_PROJECT_STATE = "Por revisar";
+    private static final String VERIFIED_PROJECT_STATE = "Verificado";
+    private static final String DECLINED_PROJECT_STATE = "Declinado";
  
     public void initialize() throws SQLException {
         fillProjectStateCombo();
@@ -51,7 +60,7 @@ public class ListProjectProposalsController {
     }
     
     public void fillProjectStateCombo() {
-        ObservableList<String > projectStates = FXCollections.observableArrayList("Todos","Por revisar","Verificados","Declinados");
+        ObservableList<String > projectStates = FXCollections.observableArrayList(ALL_COMBO_OPTION,UNVERIFIED_COMBO_OPTION,VERIFIED_COMBO_OPTION,DECLINED_COMBO_OPTION);
         comboProjectStates.setItems(projectStates);
     }
     
@@ -66,19 +75,21 @@ public class ListProjectProposalsController {
             alert.setTitle("Sin filtro");
             alert.setContentText("Debe especificar un filtro");
             alert.showAndWait();
-        } else if (Objects.equals(comboProjectStates.getSelectionModel().getSelectedItem(), "Todos")) {
-            fillUnfilteredList();
-        } else if (Objects.equals(comboProjectStates.getSelectionModel().getSelectedItem(), "Por revisar")){
-            fillFilteredProjects("Por revisar");
-        } else if (Objects.equals(comboProjectStates.getSelectionModel().getSelectedItem(), "Verificados")) {
-            fillFilteredProjects("Verificado");
-        } else {
-            fillFilteredProjects("Declinado");
+        } else{
+            if (Objects.equals(comboProjectStates.getSelectionModel().getSelectedItem(), ALL_COMBO_OPTION)) {
+                fillUnfilteredList();
+            } else if (Objects.equals(comboProjectStates.getSelectionModel().getSelectedItem(), UNVERIFIED_COMBO_OPTION)){
+                fillFilteredProjects(UNVERIFIED_PROJECT_STATE);
+            } else if (Objects.equals(comboProjectStates.getSelectionModel().getSelectedItem(), VERIFIED_COMBO_OPTION)) {
+                fillFilteredProjects(VERIFIED_PROJECT_STATE);
+            } else {
+                fillFilteredProjects(DECLINED_PROJECT_STATE);
+            }
+            labelHeader.setText(comboProjectStates.getSelectionModel().getSelectedItem());
         }
-        labelHeader.setText(comboProjectStates.getSelectionModel().getSelectedItem());
     }
     
-    public void openProjectDetails() throws SQLException {
-    
+    public void openProjectDetails() throws IOException {
+        ViewProjectProposalsApp.changeView("viewprojectdetails-view.fxml",800,600);
     }
 }
