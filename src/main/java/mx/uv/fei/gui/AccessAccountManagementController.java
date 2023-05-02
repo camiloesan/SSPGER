64528532e7304a248d77323901ea022df0965450
Luis <zs21013875@estudiantes.uv.mx>
2023-05-02 17:13:41 -0600

    clean old alerts

--- src/main/java/mx/uv/fei/gui/AccessAccountManagementController.java
+++ src/main/java/mx/uv/fei/gui/AccessAccountManagementController.java
@@ -353,23 +353,13 @@ public class AccessAccountManagementController {
         accessAccountDAO.addAdminAccessAccount(accessAccount);
     }
     
-    public boolean confirmedDeleteUser(String username) {
-        Optional<ButtonType> response = DialogGenerator.getConfirmationDialog("¿Está seguro que desea eliminar al usuario " + username + "?");
+    public boolean confirmedDeleteUser(String displayUsername) {
+        Optional<ButtonType> response = DialogGenerator.getConfirmationDialog("¿Está seguro que desea eliminar al usuario " + displayUsername + "?");
         return (response.get() == DialogGenerator.BUTTON_YES);
     }
 
     private void deleteUser(String username) throws SQLException {
         AccessAccountDAO accessAccountDAO = new AccessAccountDAO();
-        /*
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setContentText("¿Está seguro que desea eliminar al usuario " + username + "?");
-        Optional<ButtonType> result = alert.showAndWait();
-        if(result.isEmpty() || result.get() != ButtonType.OK) {
-            alert.close();
-        } else {
-            accessAccountDAO.deleteAccessAccountByUsername(username);
-        }
-        */
         if(confirmedDeleteUser(username)) {
             accessAccountDAO.deleteAccessAccountByUsername(username);
         }
@@ -381,18 +371,6 @@ public class AccessAccountManagementController {
     }
 
     private void logOut() throws IOException {
-        /*
-        LoginController.sessionDetails.cleanSessionDetails();
-        
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setContentText("¿Está seguro que desea salir, se cerrará su sesión?");
-        Optional<ButtonType> result = alert.showAndWait();
-        if(result.isEmpty() || result.get() != ButtonType.OK) {
-            alert.close();
-        } else {
-            MainStage.changeView("login-view.fxml", 600, 400 + MainStage.HEIGHT_OFFSET);
-        }
-        */
         if (confirmedLogOut()) {
             LoginController.sessionDetails.cleanSessionDetails();
             MainStage.changeView("login-view.fxml", 600, 400 + MainStage.HEIGHT_OFFSET);
