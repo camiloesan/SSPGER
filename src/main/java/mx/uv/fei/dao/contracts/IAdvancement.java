ae7f83905e20b920f62acb57e235bd4f99583a10
Luis <zs21013875@estudiantes.uv.mx>
2023-05-04 17:28:41 -0600

    get advancement details as an object

--- src/main/java/mx/uv/fei/dao/contracts/IAdvancement.java
+++ src/main/java/mx/uv/fei/dao/contracts/IAdvancement.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface IAdvancement {
     int addAdvancement(Advancement advancement) throws SQLException;
-    List<Advancement> getAdvancementDetailByName(String advancementName) throws SQLException;
+    Advancement getAdvancementDetailByName(String advancementName) throws SQLException;
     List<Advancement> getAdvancementListByProjectName(String projectName) throws SQLException;
     List<Advancement> getListAdvancementName(int professorID) throws SQLException;
     int modifyAdvancementByName(String advancementName, Advancement advancement) throws SQLException;
