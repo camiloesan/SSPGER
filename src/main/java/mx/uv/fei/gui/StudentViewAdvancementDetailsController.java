a06dc04fc2240e46fc96d27060090c6f04454ac8
Luis <zs21013875@estudiantes.uv.mx>
2023-05-04 18:39:47 -0600

    student view advancement details

---
+++ src/main/java/mx/uv/fei/gui/StudentViewAdvancementDetailsController.java
@@ -0,0 +1,91 @@
+package mx.uv.fei.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import mx.uv.fei.dao.implementations.AdvancementDAO;
+import mx.uv.fei.logic.Advancement;
+import mx.uv.fei.logic.TransferAdvancement;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class StudentViewAdvancementDetailsController implements IStudentNavigationBar{
+    @FXML
+    private Label labelAdvancementName;
+    @FXML
+    private TextFlow textAdvancementDescription;
+    @FXML
+    private Label labelStartDate;
+    @FXML
+    private Label labelDeadline;
+    @FXML
+    private HBox hboxLogOutLabel;
+    
+    public void initialize() throws SQLException {
+        getDetailedAdvancement();
+        VBox.setVgrow(hboxLogOutLabel, Priority.ALWAYS);
+    }
+    
+    public String getAdvancementName() {
+        return TransferAdvancement.getAdvancementName();
+    }
+    
+    public void getDetailedAdvancement() throws SQLException {
+        AdvancementDAO advancementDAO = new AdvancementDAO();
+        Advancement detaildedAdvancement = (advancementDAO.getAdvancementDetailByName(getAdvancementName()));
+        
+        labelAdvancementName.setText(detaildedAdvancement.getAdvancementName());
+        
+        Text advancementDescription = new Text(detaildedAdvancement.getAdvancementDescription());
+        textAdvancementDescription.getChildren().add(advancementDescription);
+        
+        labelStartDate.setText(detaildedAdvancement.getAdvancementStartDate());
+        
+        labelDeadline.setText(detaildedAdvancement.getAdvancementDeadline());
+    }
+    
+    public void returnToAdvancementList() throws IOException {
+        MainStage.changeView("studentadvancement-view.fxml", 900, 600 + MainStage.HEIGHT_OFFSET);
+    }
+    
+    @Override
+    public void redirectToAdvancements() throws IOException {
+        MainStage.changeView("studentadvancement-view.fxml", 900, 600 + MainStage.HEIGHT_OFFSET);
+    }
+    
+    @Override
+    public void redirectToEvidences() throws  IOException, SQLException {
+        MainStage.changeView("studentevidences-view.fxml", 800, 500 + MainStage.HEIGHT_OFFSET);
+    }
+    
+    @Override
+    public void redirectToProjects() throws IOException{
+        MainStage.changeView("studentviewprojects-view.fxml",900, 600 + MainStage.HEIGHT_OFFSET);
+    }
+    
+    @Override
+    public void redirectToRequest() {
+    
+    }
+    
+    public boolean confirmedLogOut() {
+        Optional<ButtonType> response = DialogGenerator.getConfirmationDialog("¿Está seguro que desea salir, se cerrará su sesión?");
+        return (response.get() == DialogGenerator.BUTTON_YES);
+    }
+    
+    @Override
+    public void actionLogOut() throws IOException {
+        LoginController.sessionDetails.cleanSessionDetails();
+        if (confirmedLogOut()) {
+            LoginController.sessionDetails.cleanSessionDetails();
+            MainStage.changeView("login-view.fxml", 600, 400 + MainStage.HEIGHT_OFFSET);
+        }
+    }
+}
