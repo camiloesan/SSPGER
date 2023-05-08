cfd0c855897e5ea4cdf0c8f31d2b01c3b74b6d33
Luis <zs21013875@estudiantes.uv.mx>
2023-05-07 21:32:51 -0600

    register project proposal new ui

--- src/main/java/mx/uv/fei/gui/RegisterProjectProposalController.java
+++ src/main/java/mx/uv/fei/gui/RegisterProjectProposalController.java
@@ -3,9 +3,10 @@ package mx.uv.fei.gui;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import mx.uv.fei.dao.implementations.ProfessorDAO;
 import mx.uv.fei.dao.implementations.ProjectDAO;
 
@@ -13,12 +14,15 @@ import mx.uv.fei.logic.AlertMessage;
 import mx.uv.fei.logic.AlertStatus;
 import mx.uv.fei.logic.Project;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public class RegisterProjectProposalController {
-    
+public class RegisterProjectProposalController implements IProfessorNavigationBar{
+    @FXML
+    private Label labelUsername;
     @FXML
     private ComboBox<String> comboAB;
     @FXML
@@ -49,14 +53,18 @@ public class RegisterProjectProposalController {
     private TextArea textAreaExpectedResults;
     @FXML
     private TextArea textAreaRecommendedBibliography;
+    @FXML
+    private HBox hboxLogOutLabel;
     
     
     public void initialize() throws SQLException {
+        labelUsername.setText(LoginController.sessionDetails.getUsername());
         fillLgacCombo();
         fillProfessorsCombos();
         fillReceptionWorkModalityCombo();
         fillStudentsCombo();
         fillABcombo();
+        VBox.setVgrow(hboxLogOutLabel, Priority.ALWAYS);
     }
     
     public void fillLgacCombo() throws SQLException {
@@ -202,5 +210,39 @@ public class RegisterProjectProposalController {
         projectDAO.addProject(project);
         projectDAO.setDirectorIDtoProject(project);
         projectDAO.setCodirectorIDtoProject(project);
+    }
+    
+    @Override
+    public void redirectToAdvancementManagement() throws IOException {
+        MainStage.changeView("advancementsmanagement-view.fxml",1000,600 + MainStage.HEIGHT_OFFSET);
+    }
+    
+    @Override
+    public void redirectToProjectManagement() throws IOException {
+        MainStage.changeView("registerprojectproposal-view.fxml",1000,600 + MainStage.HEIGHT_OFFSET);
+    }
+    
+    @Override
+    public void redirectToEvidences() throws IOException {
+        MainStage.changeView("professorevidences-view.fxml", 1000, 600 + MainStage.HEIGHT_OFFSET);
+    }
+    
+    @Override
+    public void redirectToRequests() throws IOException {
+        MainStage.changeView("projectrequests-view.fxml", 1000, 600 + MainStage.HEIGHT_OFFSET);
+    }
+    
+    public boolean confirmedLogOut() {
+        Optional<ButtonType> response = DialogGenerator.getConfirmationDialog("¿Está seguro que desea salir, se cerrará su sesión?");
+        return (response.get() == DialogGenerator.BUTTON_YES);
+    }
+    
+    @Override
+    public void actionLogOut() throws IOException {
+        LoginController.sessionDetails.cleanSessionDetails();
+        if (confirmedLogOut()) {
+            LoginController.sessionDetails.cleanSessionDetails();
+            MainStage.changeView("login-view.fxml", 600, 400 + MainStage.HEIGHT_OFFSET);
+        }
     }
 }
