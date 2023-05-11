fea3e5c65aa11a0f029b583a6ba5f82f1b88599e
Bryam Danae <bryamdmg@gmail.com>
2023-05-11 16:53:06 -0600

    foreign keys on update cascade

--- SQLScripts/FK_SSPGER.sql
+++ SQLScripts/FK_SSPGER.sql
@@ -1,30 +1,30 @@
 USE SSPGER;
 
 ALTER TABLE Estudiantes
-    ADD CONSTRAINT FK_Estudiantes_CuentasAcceso FOREIGN KEY (nombreUsuario) REFERENCES CuentasAcceso (nombreUsuario) ON DELETE CASCADE;
+    ADD CONSTRAINT FK_Estudiantes_CuentasAcceso FOREIGN KEY (nombreUsuario) REFERENCES CuentasAcceso (nombreUsuario) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE Profesores ADD
-    CONSTRAINT FK_Profesores_CuentasAcceso FOREIGN KEY (nombreUsuario) REFERENCES CuentasAcceso (nombreUsuario) ON DELETE CASCADE;
+    CONSTRAINT FK_Profesores_CuentasAcceso FOREIGN KEY (nombreUsuario) REFERENCES CuentasAcceso (nombreUsuario) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE CuerpoAcademico ADD
-    CONSTRAINT FK_CuerpoAcademico_Profesores FOREIGN KEY (responsable) REFERENCES Profesores (ID_profesor) ON DELETE CASCADE;
+    CONSTRAINT FK_CuerpoAcademico_Profesores FOREIGN KEY (responsable) REFERENCES Profesores (ID_profesor) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE Proyectos
-    ADD CONSTRAINT FK_Proyectos_LGAC FOREIGN KEY (LGAC) REFERENCES LGAC (ID_lgac) ON DELETE CASCADE,
-    ADD CONSTRAINT FK_Proyectos_ModalidadesTR FOREIGN KEY (ID_modalidadTR) REFERENCES ModalidadesTR (ID_modalidadTR) ON DELETE CASCADE,
-    ADD CONSTRAINT FK_Proyectos_Directores FOREIGN KEY (ID_director) REFERENCES Profesores (ID_profesor) ON DELETE CASCADE,
-    ADD CONSTRAINT FK_Proyectos_Codirectores FOREIGN KEY (ID_codirector) REFERENCES Profesores (ID_profesor) ON DELETE CASCADE;
+    ADD CONSTRAINT FK_Proyectos_LGAC FOREIGN KEY (LGAC) REFERENCES LGAC (ID_lgac) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT FK_Proyectos_ModalidadesTR FOREIGN KEY (ID_modalidadTR) REFERENCES ModalidadesTR (ID_modalidadTR) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT FK_Proyectos_Directores FOREIGN KEY (ID_director) REFERENCES Profesores (ID_profesor) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT FK_Proyectos_Codirectores FOREIGN KEY (ID_codirector) REFERENCES Profesores (ID_profesor) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE Avances
-    ADD CONSTRAINT FK_Avances_Proyectos FOREIGN KEY (ID_proyecto) REFERENCES Proyectos (ID_proyecto) ON DELETE CASCADE;
+    ADD CONSTRAINT FK_Avances_Proyectos FOREIGN KEY (ID_proyecto) REFERENCES Proyectos (ID_proyecto) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE Evidencias
-    ADD CONSTRAINT FK_Evidencias_Estudiantes FOREIGN KEY (matriculaEstudiante) REFERENCES Estudiantes (matricula) ON DELETE CASCADE;
+    ADD CONSTRAINT FK_Evidencias_Estudiantes FOREIGN KEY (matriculaEstudiante) REFERENCES Estudiantes (matricula) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE SolicitudesProyecto
-    ADD CONSTRAINT FK_SolicitudesProyecto_Proyectos FOREIGN KEY (ID_proyecto) REFERENCES Proyectos (ID_proyecto) ON DELETE CASCADE,
-    ADD CONSTRAINT FK_SolicitudesProyecto_Estudiantes FOREIGN KEY (matriculaEstudiante) REFERENCES Estudiantes (matricula) ON DELETE CASCADE;
+    ADD CONSTRAINT FK_SolicitudesProyecto_Proyectos FOREIGN KEY (ID_proyecto) REFERENCES Proyectos (ID_proyecto) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT FK_SolicitudesProyecto_Estudiantes FOREIGN KEY (matriculaEstudiante) REFERENCES Estudiantes (matricula) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE ProyectosEstudiantes
-    ADD CONSTRAINT FK_ProyectosEstudiantes_Proyectos FOREIGN KEY(ID_proyecto) REFERENCES Proyectos(ID_proyecto),
-    ADD CONSTRAINT FK_ProyectosEstudiantes_Estudiantes FOREIGN KEY(matriculaEstudiante) REFERENCES Estudiantes(matricula);
+    ADD CONSTRAINT FK_ProyectosEstudiantes_Proyectos FOREIGN KEY(ID_proyecto) REFERENCES Proyectos(ID_proyecto) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT FK_ProyectosEstudiantes_Estudiantes FOREIGN KEY(matriculaEstudiante) REFERENCES Estudiantes(matricula) ON DELETE CASCADE ON UPDATE CASCADE;
