6db0956b6e9bd77ec9d9178b1332ac41cb67931d
Luis <zs21013875@estudiantes.uv.mx>
2023-05-13 14:05:05 -0600

    new class for listing projects

---
+++ src/main/java/mx/uv/fei/logic/SimpleProject.java
@@ -0,0 +1,22 @@
+package mx.uv.fei.logic;
+
+import java.util.Objects;
+
+public class SimpleProject extends DetailedProject{
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof SimpleProject)) {
+            return false;
+        }
+        SimpleProject simpleProject = (SimpleProject) obj;
+        return getProjectID() == simpleProject.getProjectID() && Objects.equals(getProjectTitle(), simpleProject.getProjectTitle());
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(getProjectID(), getProjectTitle());
+    }
+}
