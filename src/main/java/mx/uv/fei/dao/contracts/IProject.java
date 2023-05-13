61db766a6c61a035b1d5c2d78e38f7ae7de8f267
Luis <zs21013875@estudiantes.uv.mx>
2023-05-13 14:07:00 -0600

    project dao normal flow tests

--- src/main/java/mx/uv/fei/dao/contracts/IProject.java
+++ src/main/java/mx/uv/fei/dao/contracts/IProject.java
@@ -2,6 +2,7 @@ package mx.uv.fei.dao.contracts;
 
 import mx.uv.fei.logic.DetailedProject;
 import mx.uv.fei.logic.Project;
+import mx.uv.fei.logic.SimpleProject;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -13,8 +14,8 @@ public interface IProject {
     int setCodirectorIDtoProject(Project projectCodirectorName) throws SQLException;
     int updateProjectState(int projectId, String state) throws SQLException;
     int getProjectIDByTitle(String title) throws SQLException;
-    ArrayList<DetailedProject> getProjectsByState(String projectState) throws SQLException;
-    List<DetailedProject> getAllProjects() throws SQLException; //similar to getProjectsByState but to get all projects no matter the status of the project
+    List<SimpleProject> getProjectsByState(String projectState) throws SQLException;
+    List<SimpleProject> getAllProjects() throws SQLException; //similar to getProjectsByState but to get all projects no matter the status of the project
     List<DetailedProject> getProjectsByRole(int professorID) throws SQLException;
     DetailedProject getProjectInfoByID(int projectID) throws SQLException;
     List<String> getLgacList() throws SQLException;
